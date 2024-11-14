2b24b0ae35a092bff35bd9ed81eb19b0cebba911
Jacob-Chmura <jacobpaul.chmura@gmail.com>
2024-11-14 17:17:18 -0500

    Add numpy dependency

--- ml_boilerplate/main.py
+++ ml_boilerplate/main.py
@@ -2,9 +2,17 @@ def hello_world() -> str:
     return "Hello World"
 
 
+def hello_numpy() -> str:
+    import numpy as np
+
+    _ = np.zeros(1337)
+    return "Hello Numpy"
+
+
 def main() -> None:
     print("Checking packages...", end="")
     hello_world()
+    hello_numpy()
     print("Ok.")
 
 
